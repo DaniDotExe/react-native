e32b941ea28cde4f582600c5f308177a17db11cf
Dani <odos26@hotmail.com>
2022-02-18 21:54:51 -0500

    Clase 11

--- 01-ts-react/src/typescript/TiposBasicos.tsx
+++ 01-ts-react/src/typescript/TiposBasicos.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 export const TiposBasicos = () => {
+
+  const nombre: string = 'Oscar'; 
+  const edad: number = 23;
+  let estaActivo: boolean = true;
+
+  const poderes:any[] = [];
+
+  poderes.push('1717');
+  
+
   return (
     <>
         <h3>Tipos básicos</h3>
+        {nombre}, {edad}, { (estaActivo) ? 'estamos True' : 'Estamos no True'}
+        <br />
+        {poderes.join(', ')}
     </>
   )
 }
