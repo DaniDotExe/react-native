7af05034da54bfbfe922db267fa86f3691b03be9
Dani <odos26@hotmail.com>
2022-02-26 02:19:23 -0500

    Clase 22

--- 01-ts-react/src/components/Usuarios.tsx
+++ 01-ts-react/src/components/Usuarios.tsx
@@ -5,18 +5,48 @@ import { ReqResListado, Usuario } from '../interfaces/reqRes';
 
 export const Usuarios = () => {
   
-  const [Usuarios, setUsuarios] = useState<Usuario[]>([]);
+  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
 
   useEffect(() => {
     // llamado al API
     reqRestApi.get<ReqResListado>('/users')
       .then( resp => {
-        console.log( resp.data.data );
+        setUsuarios( resp.data.data)
       })
       .catch( console.log );
 
   }, [])
+
+  //dos formas de hacerlo
+  //const renderItem = ( usuario: Usuario)  => {
+  //  return (
+  //    <tr key= { usuario.id.toString() }>
+  //        <th> </th>
+  //        <th>{ usuario.first_name }</th>
+  //        <th>email</th>
+  //    </tr>
+  //  )
+  //}
   
+  const renderItem = ( {id, first_name, last_name, email, avatar}: Usuario)  => {
+    return (
+      <tr key= { id.toString() }>
+          <th>
+              <img src={avatar } 
+              //en caso de que no exista la imagen se usa alt para colocar algo por defecto
+              alt={ first_name } 
+              style={{
+                width: 60,
+                borderRadius: 100,
+              }}
+              
+              />
+          </th>
+          <th> { first_name } { last_name } </th>
+          <th> { email }</th>
+      </tr>
+    )
+  }
 
   return (
     <>
@@ -31,9 +61,18 @@ export const Usuarios = () => {
             </thead>
 
             <tbody>
-
+              {
+                //Dos formas de escribirlo
+                //usuarios.map( usuario => renderItem(usuario) )
+                usuarios.map(renderItem)
+              }
             </tbody>
         </table>
+        <button
+          className='btn btn-primary'
+        >   
+          Siguientes
+        </button>
     </>
   )
 }
