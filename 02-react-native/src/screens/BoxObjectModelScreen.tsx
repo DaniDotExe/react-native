85f5f9555f24938c91d419cf3af31b3c42871bfc
Dani <odos26@hotmail.com>
2022-03-26 15:32:51 -0500

    Clase 67

--- 02-react-native/src/screens/BoxObjectModelScreen.tsx
+++ 02-react-native/src/screens/BoxObjectModelScreen.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export const BoxObjectModelScreen = () => {
   return (
-    <View>
-        <Text>Box</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>Box</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: 'red',
+      flex: 1,
+    },
+    title: {
+      fontSize: 20,
+      margin: 10,
+      padding: 15,
+      paddingTop: 45,
+      borderWidth: 20,
+      textAlign: 'center',
+    },
+});
  
