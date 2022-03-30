eb8040f5e25ffb0e1b9308b52897b4a426ebac12
Dani <odos26@hotmail.com>
2022-03-30 17:03:00 -0500

    Clase 90

--- 03-calculator/src/components/BotonCalc.tsx
+++ 03-calculator/src/components/BotonCalc.tsx
@@ -1,23 +1,39 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { styles } from '../theme/appTheme';
 
 interface Props {
     texto: string,
     color?: string,
+    ancho?: boolean,
 }
 
 const naranjaColor = '#FF9427'
 const grisColor = '#9B9B9B'
 
-export const BotonCalc = ({texto = 'C', color = '#2D2D2D'}: Props) => {
+export const BotonCalc = ({texto = 'C', color = '#2D2D2D', ancho = false}: Props) => {
   return (
-    <View style={{
-        ...styles.boton,
-        backgroundColor: color,
-    }}>
-        <Text style={styles.botonTexto}>{texto}</Text>
-    </View>
+    <TouchableOpacity>
+        <View style={[
+            styles.boton,
+            {backgroundColor: color,
+            width: (ancho===true) ? 180 : styles.boton.width
+            }
+        ]}>
+            {/* <Text style={[
+                styles.botonTexto,
+                (color === '#9B9B9B')
+                    ? {color: 'black'}
+                    : {color: 'white'}
+            ]}
+            > */}
+            <Text style={{
+                ...styles.botonTexto,
+                color: ( color === '#9B9B9B') ? 'black' : 'white'
+            }}>
+            {texto}</Text>
+        </View>
+    </TouchableOpacity>
   )
 }
 
