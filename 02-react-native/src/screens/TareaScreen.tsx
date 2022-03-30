4c4463da7de716cd3d11bf2b44e6eaca786debf3
Dani <odos26@hotmail.com>
2022-03-29 23:28:00 -0500

    Seccion 6 - Creacion del proyecto para la calculadora

--- 02-react-native/src/screens/TareaScreen.tsx
+++ 02-react-native/src/screens/TareaScreen.tsx
@@ -14,27 +14,36 @@ export const TareaScreen = () => {
 const styles = StyleSheet.create({
     container:  {
         flex: 1,
-        backgroundColor: '#28425B'
+        backgroundColor: '#28425B',
+        flexDirection:'row',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     cajaMorada: {
         width: 100,
         height: 100,
         borderWidth: 10,
         borderColor: 'white',
-        backgroundColor: '#5856D6'
+        backgroundColor: '#5856D6',
+        
+        
     },
     cajaNaranja: {
         width: 100,
         height: 100,
         borderWidth: 10,
         borderColor: 'white',
-        backgroundColor: '#F0A23B'
+        backgroundColor: '#F0A23B',
+        top: 50
+        
+        
     },
     cajaAzul: {
         width: 100,
         height: 100,
         borderWidth: 10,
         borderColor: 'white',
-        backgroundColor: '#28C4D9'
+        backgroundColor: '#28C4D9',
+        
     },
 });
