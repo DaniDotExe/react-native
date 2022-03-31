e222c23130f9030cc0f9970227495c86dcb8b188
Dani <odos26@hotmail.com>
2022-03-30 19:28:39 -0500

    Clase 104

--- 04-NavegacionApp/App.tsx
+++ 04-NavegacionApp/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import 'react-native-gesture-handler';
 
-export const App = () => {
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './src/navigator/StackNavigator';
+
+const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigator/>
+    </NavigationContainer>
   )
 }
 
