00018148df030cefd1e1fac24c50e518319aa515
DaniSTexe <odos26@hotmail.com>
2022-04-21 23:15:17 -0500

    Clase 123

--- 04-NavegacionApp/src/navigator/Tabs.tsx
+++ 04-NavegacionApp/src/navigator/Tabs.tsx
@@ -3,7 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Tab1Screen } from '../screens/Tab1Screen';
 import { Tab2Screen } from '../screens/Tab2Screen';
 import { StackNavigator } from './StackNavigator';
-import { useColorScheme } from 'react-native';
+import { Text, useColorScheme } from 'react-native';
 import { colores } from '../theme/appTheme';
 
 const Tab = createBottomTabNavigator();
@@ -15,14 +15,40 @@ export const Tabs = () => {
             backgroundColor: 'red'
          }}
         
-        screenOptions = {{
+        screenOptions = { ({ route }) => ({
             tabBarActiveTintColor: colores.primary,
             tabBarStyle: {
                 borderTopWidth: 0,
                 elevation: 0,
             },
-        }}
+            tabBarLabelStyle: {
+                fontSize: 15
+            },
+            tabBarIcon: ({ color, focused, size }) => {
+
+                let iconName: string = '';
+
+                switch( route.name ){
+                    case 'Tab1Screen':
+                        iconName = 'T1'
+                    break;
+
+                    case 'Tab2Screen':
+                        iconName = 'T2'
+                    break;
+
+                    case 'StackNavigator':
+                        iconName = 'St'
+                    break;
+                }
+
+                console.log(route.name)
+                return <Text style={{ color }}>{ iconName }</Text>
+            }
+        })
+    }
     >
+      {/* <Tab.Screen name="Tab1Screen" options={{ title: 'Tab1', tabBarIcon: (props) => <Text style={{ color: props.color }}>T1</Text>}} component={Tab1Screen} /> */}
       <Tab.Screen name="Tab1Screen" options={{ title: 'Tab1'}} component={Tab1Screen} />
       <Tab.Screen name="Tab2Screen" options={{ title: 'Tab2'}}component={Tab2Screen} />
       <Tab.Screen name="StackNavigator" options={{ title: 'Stack'}} component={StackNavigator} />
